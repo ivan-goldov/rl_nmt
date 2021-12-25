8addd108175729891b3d6b8c96f66f5b8e2ac09c
Ivan Goldov <ivan.goldov@pm.me>
2021-12-25 18:05:49 +0300

    Fix train

--- src/training/training.py
+++ src/training/training.py
@@ -1,11 +1,11 @@
-from typing import Tuple
+from typing import Tuple, Optional
 
 import torch
 from datasets import load_dataset
 from torch import nn, Tensor
 from torch.utils.data import DataLoader
 from tqdm import tqdm
-from transformers import AutoTokenizer, GPT2TokenizerFast, FSMTTokenizer
+from transformers import GPT2TokenizerFast
 
 from src.modules.transformer_translation_model import Seq2SeqTransformer
 
@@ -28,13 +28,14 @@ def create_mask(src: Tensor, tgt: Tensor, device, pad_idx: int = 1) -> Tuple[Ten
     return src_mask, tgt_mask, src_padding_mask, tgt_padding_mask
 
 
-def train(model: torch.nn.Module = None,
+def train(model: Optional[torch.nn.Module] = None,
           epochs: int = 20,
-          batch_size: int = 32,
+          batch_size: int = 2,
           lr: float = 3e-4,
           device: torch.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu'),
           pad_idx: int = 1):
-    model = Seq2SeqTransformer()
+    if not model:
+        model = Seq2SeqTransformer()
     model.to(device)
     model.train()
 
@@ -42,14 +43,14 @@ def train(model: torch.nn.Module = None,
         if param.dim() > 1:
             nn.init.xavier_uniform_(param)
 
+    tokenizer = GPT2TokenizerFast.from_pretrained('../nmt_tokenizer')
+    tokenizer.pad_token = tokenizer.eos_token
+    pad_idx = tokenizer.eos_token_id
+
     criterion = torch.nn.CrossEntropyLoss(ignore_index=pad_idx)
     optimizer = torch.optim.Adam(model.parameters(), lr=lr)
 
     dataset = load_dataset('opus_books', 'en-ru', split='train')['translation'][:2]
-    # tokenizer = GPT2TokenizerFast.from_pretrained('../nmt_tokenizer')
-
-    mname = "facebook/wmt19-en-ru"
-    tokenizer = FSMTTokenizer.from_pretrained(mname)
 
     train_dataloader = DataLoader(dataset, batch_size=batch_size)
 
@@ -59,17 +60,17 @@ def train(model: torch.nn.Module = None,
             for d in train_dataloader:
                 src = d['en']
                 tgt = d['ru']
-                src = tokenizer(src, return_tensors='pt', padding='max_length', truncation=True, max_length=32)
-                tgt = tokenizer(tgt, return_tensors='pt', padding='max_length', truncation=True, max_length=32)
+                src = tokenizer(src, return_tensors='pt', padding='max_length', truncation=True, max_length=64)
+                tgt = tokenizer(tgt, return_tensors='pt', padding='max_length', truncation=True, max_length=64)
 
                 src_ids = src['input_ids'].transpose(1, 0).to(device)
-                # src_mask = src['attention_mask'].bool().reshape((batch_size, -1)).to(device)
                 tgt_ids = tgt['input_ids'].transpose(1, 0).to(device)
                 src_mask, tgt_mask, src_padding_mask, tgt_padding_mask = create_mask(src_ids, tgt_ids, device)
 
-                logits = model(src_ids, tgt_ids)
-                # logits = model(src_ids, tgt_ids, src_mask, tgt_mask, src_padding_mask, tgt_padding_mask, src_padding_mask)
+                logits = model(src_ids, tgt_ids, src_mask, tgt_mask, src_padding_mask, tgt_padding_mask,
+                               src_padding_mask)
                 loss = criterion(logits.reshape(-1, logits.shape[-1]), tgt_ids.reshape(-1))
+                print(torch.argmax(logits, dim=0))
                 loss.backward()
                 torch.save(model.state_dict(), './checkpoint_model')
                 epoch_loss += loss.item()
@@ -79,27 +80,25 @@ def train(model: torch.nn.Module = None,
             pbar.update(1)
 
 
-# def greedy_decode(model, src, src_mask, max_len, start_symbol):
-#     src = src.to(DEVICE)
-#     src_mask = src_mask.to(DEVICE)
-#
-#     memory = model.encode(src, src_mask)
-#     ys = torch.ones(1, 1).fill_(start_symbol).type(torch.long).to(DEVICE)
-#     for i in range(max_len - 1):
-#         memory = memory.to(DEVICE)
-#         tgt_mask = (generate_square_subsequent_mask(ys.size(0))
-#                     .type(torch.bool)).to(DEVICE)
-#         out = model.decode(ys, memory, tgt_mask)
-#         out = out.transpose(0, 1)
-#         prob = model.generator(out[:, -1])
-#         _, next_word = torch.max(prob, dim=1)
-#         next_word = next_word.item()
-#
-#         ys = torch.cat([ys,
-#                         torch.ones(1, 1).type_as(src.data).fill_(next_word)], dim=0)
-#         if next_word == EOS_IDX:
-#             break
-#     return ys
+def greedy_decode(model: nn.Module, src: Tensor, src_mask: Tensor, max_len: int, start_symbol: str, device):
+    src = src.to(device)
+    src_mask = src_mask.to(device)
+
+    memory = model.encode(src, src_mask)
+    ys = torch.ones(1, 1).fill_(start_symbol).to(torch.long).to(device)
+    for i in range(max_len - 1):
+        memory = memory.to(device)
+        tgt_mask = (generate_square_subsequent_mask(ys.size(0), device)
+                    .type(torch.bool)).to(device)
+        out = model.decode(ys, memory, tgt_mask)
+        out = out.transpose(0, 1)
+        prob = model.generator(out[:, -1])
+        _, next_word = torch.max(prob, dim=1)
+        next_word = next_word.item()
+
+        ys = torch.cat([ys,
+                        torch.ones(1, 1).type_as(src.data).fill_(next_word)], dim=0)
+    return ys
 
 
 def main():
